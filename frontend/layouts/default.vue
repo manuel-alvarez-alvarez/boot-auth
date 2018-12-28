d28b96e70d48fe828ce9341553b4cf50a00f74da
malvarez <manuel.alvarez.alvarez@gmail.com>
2018-12-28 10:08:25 +0100

    Add nuxt for a proper frontend

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="false"
+      v-model="drawer"
+      fixed
+      app>
+      <v-list>
+        <v-list-tile
+          v-for="(item, i) in items"
+          :to="item.to"
+          :key="i"
+          router
+          exact>
+          <v-list-tile-action>
+            <v-icon v-html="item.icon" />
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="false"
+      fixed
+      app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
+      </v-btn>
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="true"
+      app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        drawer: true,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+        ],
+        miniVariant: true,
+        title: 'Authentication'
+      }
+    }
+  }
+</script>
---
+++ frontend/middleware/auth.js
@@ -0,0 +1,10 @@
+export default function (ctx) {
+  if(!isAuth()) {
+    return ctx.redirect('/login')
+  }
+}
+
+function isAuth() {
+  // Check if user session exists somehow
+  return false;
+}
